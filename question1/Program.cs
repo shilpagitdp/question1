5ed29c4d6a7ea41573da1bfe70d664e3f0d7588a
Shilpa.Devanga <Shilpa.Devanga@INLPF2KZ387>
2022-09-30 20:22:34 +0530

    Add project files.

---
+++ question1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eval1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+            Console.WriteLine("Please enter 6 nums: ");
+            for (int i = 0; i < 6; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int max = array[0];
+            int min = array[0];
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            Console.WriteLine("maximum value is : " + max);
+            Console.WriteLine("minimum value is : " + min);
+        }
+    }
+
+}
